44362b44a11c89d3311c2204583104df07f93baf
Karl Prieb <karl@prieb.me>
2022-04-21 19:07:12 -0300

    chore(metrics): log web vitals metrics locally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import type { AppProps } from 'next/app';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
+}
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+	if (process.env.NODE_ENV === 'development') console.log(metric);
 }
 
 export default MyApp;
