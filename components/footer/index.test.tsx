6b2ac69cdd5df1d350652187514208c0ef86e76a
Karl Prieb <karl@ardrive.io>
2022-04-28 17:52:23 -0300

    test(footer): better readability
    Co-authored-by: Derek Sonnenberg <fedellen@pixelpajamastudios.com>

--- components/footer/index.test.tsx
+++ components/footer/index.test.tsx
@@ -3,7 +3,7 @@ import Footer from '@components/footer';
 
 describe('Footer', () => {
 	it('renders last updated', () => {
-		const minutesAgo = new Date().getTime() - 1800000;
+		const thirtyMinutesAgo = new Date().getTime() - 1_800_000;
 		const { container } = render(<Footer lastUpdated={minutesAgo} />);
 
 		const text = screen.getByText('30 minutes ago');
